c4cfc2364daf0720517a6b6f255cf7c5ceb8750f
Thai Huynh <rafehuynh0510@gmail.com>
2024-06-10 19:53:19 -0500

    completed right side bar

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -7,10 +7,13 @@ import Carousel from './Carousel';
 import Header from './Header';
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './ui/LoaderSpinner';
 const RightSideBar = () => {
   const {user} = useUser();
   const topPodcast = useQuery(api.users.getTopUserByPodcastCount)
-
+  const router = useRouter()
+  if (!topPodcast) return <LoaderSpinner />
   return (
     <section className='right_sidebar text-white-1'>
         <SignedIn>
@@ -28,6 +31,23 @@ const RightSideBar = () => {
           
           />
           <Carousel topPodcast = {topPodcast!}/>
+        </section>
+        <section className='flex flex-col gap-8 pt-12'>
+      <Header headerTitle='Top Podcasters'/>
+        <div className='flex flex-col gap-6'>
+        {topPodcast?.slice(0,4).map((item) => (
+          <div key={item._id} className='flex cursor-pointer justify-between' onClick={() => router.push(`/profile/${item.clerkId}`) }>
+            <figure className='flex items-center gap-2'>
+              <Image src={item.imageUrl} alt={item.name} width={44} height={44} className='aspect-square rounded-lg'/>
+              <h2 className='text-14 font-semibold text-white-1'>{item.name} </h2>
+            </figure>
+            <div className='flex items-center'>
+              <p className='text-12 font-normal'>{item.totalPodcasts} podcasts</p>
+              </div>
+            </div>
+        ))
+        }
+        </div>
         </section>
     </section>
   )
