ba5d304fd059e8462009563096c5d614ad613616
Thai Huynh <rafehuynh0510@gmail.com>
2024-06-13 14:50:56 -0500

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        typescript:{
+        typescript: {
             ignoreBuildErrors: true
-        },
+          },
         remotePatterns: [
             {
                 protocol: "https",
